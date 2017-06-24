6e27d0e18f18d888195f8d871747bbf288c707f2
Tyler Findlay <tyler@tylerfindlay.com>
2017-06-24 14:15:22 -0600

    Added some new algorithms to try

--- SMART Lead Forms/SMART Lead Forms/contextual_bandit.py
+++ SMART Lead Forms/SMART Lead Forms/contextual_bandit.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
+import tensorflow.contrib.layers as slim
 import numpy as np
 
 class contextual_bandit():
@@ -26,20 +26,21 @@ class contextual_bandit():
             return -1
 
 class agent():
-    def __init__(self, lr, s_size,a_size):
+    def __init__(self, lr, s_size, a_size, f_size, state_size):
         #These lines established the feed-forward part of the network. The agent takes a state and produces an action.
-        self.state_in= tf.placeholder(shape=[1],dtype=tf.int32)
+        self.state_in= tf.placeholder(shape=[state_size],dtype=tf.int32)
         state_in_OH = slim.one_hot_encoding(self.state_in,s_size)
         output = slim.fully_connected(state_in_OH,a_size,\
             biases_initializer=None,activation_fn=tf.nn.sigmoid,weights_initializer=tf.ones_initializer())
-        self.output = tf.reshape(output,[-1])
-        self.chosen_action = tf.argmax(self.output,0)
+        self.output = tf.reshape(output,f_size)
+        self.chosen_action = tf.argmax(self.output,f_size)
 
         #The next six lines establish the training proceedure. We feed the reward and chosen action into the network
         #to compute the loss, and use it to update the network.
         self.reward_holder = tf.placeholder(shape=[1],dtype=tf.float32)
-        self.action_holder = tf.placeholder(shape=[1],dtype=tf.int32)
-        self.responsible_weight = tf.slice(self.output,self.action_holder,[1])
+        self.action_holder = tf.placeholder(shape=[f_size],dtype=tf.int32)
+
+        self.responsible_weight = tf.slice(self.output,self.action_holder,tf.ones([f_size], dtype=tf.int32))
         self.loss = -(tf.log(self.responsible_weight)*self.reward_holder)
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
         self.update = optimizer.minimize(self.loss)
@@ -47,7 +48,7 @@ class agent():
 tf.reset_default_graph() #Clear the Tensorflow graph.
 
 cBandit = contextual_bandit() #Load the bandits.
-myAgent = agent(lr=0.001,s_size=cBandit.num_bandits,a_size=cBandit.num_actions) #Load the agent.
+myAgent = agent(lr=0.001,s_size=cBandit.num_bandits,a_size=cBandit.num_actions, f_size=5, state_size=1) #Load the agent.
 weights = tf.trainable_variables()[0] #The weights we will evaluate to look into the network.
 
 total_episodes = 10000 #Set total number of episodes to train agent on.
@@ -80,6 +81,7 @@ with tf.Session() as sess:
         if i % 500 == 0:
             print("Mean reward for each of the " + str(cBandit.num_bandits) + " bandits: " + str(np.mean(total_reward,axis=1)))
         i+=1
+
 for a in range(cBandit.num_bandits):
     print("The agent thinks action " + str(np.argmax(ww[a])+1) + " for bandit " + str(a+1) + " is the most promising....")
     if np.argmax(ww[a]) == np.argmin(cBandit.bandits[a]):
